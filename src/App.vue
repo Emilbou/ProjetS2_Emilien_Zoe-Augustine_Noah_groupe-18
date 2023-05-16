ad02b6a3f1da97557279b9898f0678f8cf1bea5f
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2023-05-16 16:02:40 +0200

    template header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import TestComponent from './components/TestComponent.vue';
 import Footer from './components/Footer.vue';
+import Header from './components/Header.vue';
 </script>
 
 <template>
+  <Header/>
   <p class="bg-yellow text-2xl font-poppins">test</p>
   <div class="flex px-3">
     <TestComponent class="mx-5 items-center content-center" />
-
+<RouterView/>
   </div>
-<Footer />
+<Footer/>
 </template>
--- src/pages/TestPage2.vue
+++ src/pages/TestPage2.vue
@@ -6,4 +6,5 @@
     <header>
         <h1 class="text-4xl">Test</h1>
     </header>
+
 </template>

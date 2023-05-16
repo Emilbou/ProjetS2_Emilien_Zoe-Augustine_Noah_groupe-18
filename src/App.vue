77d37ac0840b32527f325dfc716a9fc02b331370
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2023-05-16 08:31:22 +0200

    import poppins et début europa grotesk

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@ import Footer from './components/Footer.vue';
 </script>
 
 <template>
-  <p class="bg-yellow text-2xl">test</p>
+  <p class="bg-yellow text-2xl font-poppins">test</p>
   <div class="flex px-3">
     <TestComponent class="mx-5 items-center content-center" />
 
   </div>
-  <Footer />
+<Footer />
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,9 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      
+      fontFamily: {
+      sans: ['Poppins', 'sans-serif']
+    },
  colors : {
   "white": "#ffffff",
   "beige": "#fffaef",

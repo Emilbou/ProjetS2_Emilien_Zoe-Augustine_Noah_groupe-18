06782e2dec22e539ad51f0e3b6ff88b320e86514
Lafourcade-Noah <116797848+Lafourcade-Noah@users.noreply.github.com>
2023-05-19 23:04:36 +0200

    lien router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import AproposVue from '@/Apropos.vue'
-import AppVue from '@/App.vue'
+// Quand tu crée une page qui doit s'afficher, tu l'importes comme ça
+
+import HomeView from '../pages/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+
+    // et ensuite tu la déclare comme ceci avec le nom dans 
+    // le composant qui est le même nom que tu lui donnes à l'appel plus haut
     {
       path: '/',
-      name: 'appvue',
-      component: AppVue
+      name: 'home',
+      component: HomeView
     },
     {
-    path: '/apropos',
-    name: 'apropos',
-    component: AproposVue
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../pages/AboutView.vue')
     }
-
   ]
 })
 

be57ef8ba6417786366309eb5365088cc6b95d30
Lafourcade-Noah <116797848+Lafourcade-Noah@users.noreply.github.com>
2023-05-23 16:02:37 +0200

    texte about

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -3,5 +3,9 @@
 </script>
 
 <template>
-    <p>4vdgbrthbrsyhsbf</p>
+    <p class="text-green-dull grids-span-4">Rue des Saveurs est le fruit d’un projet collectif au sein du département MMi (Métiers du Multimédia et d’Internet) de l’IUT Nord Franche-Comté. 
+
+Réalisé par une équipe de trois personnes, nous comptons parmi nos membres Emilien Boucard, s’occupant de l’intégration web, Zoe-Augustine Vicat du design et Noah Lafourcade, chargé de communication. 
+
+Nous sommes fiers de vous présenter notre projet Rue des Saveurs, dont le but est avant tout le partage : profitant aux particuliers comme aux producteurs, notre site est là pour vous permettre de vendre et d’acheter les produits frais au sein de votre voisinnage. Nous vous serions reconnaissant de nous suivre sur nos différents réseaux sociaux.</p>
 </template>

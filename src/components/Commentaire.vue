9ea6b8ecb9ca907b386a22f29391f5ffd8002321
emilbou <emilien.boucard@edu.univ-fcomte.fr>
2023-05-24 16:12:31 +0200

    component commentaire

---
+++ src/components/Commentaire.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+
+</script>
+
+<div class="bg-beige p-4 rounded-md shadow-md" style="box-shadow: -6px 4px 5px 0px rgba(0, 0, 0, 0.1);">
+    <div class="flex items-center mb-4">
+      <img src="CHEMIN_DE_L_IMAGE" alt="Image de profil" class="w-12 h-12 rounded-full mr-4">
+      <div>
+        <h4 class="text-lg font-bold">NOM_PRÉNOM</h4>
+        <div class="flex items-center mt-1">
+          <svg class="w-4 h-4 fill-current text-yellow-500" viewBox="0 0 20 20">
+            <path
+              d="M10 0L12.2456 6.17508H19.6932L13.7679 10.0343L15.6139 16.8249L10 13.3657L4.38608 16.8249L6.23207 10.0343L0.306796 6.17508H7.75442L10 0Z"
+            />
+          </svg>
+          <svg class="w-4 h-4 fill-current text-yellow-500" viewBox="0 0 20 20">
+            <path
+              d="M10 0L12.2456 6.17508H19.6932L13.7679 10.0343L15.6139 16.8249L10 13.3657L4.38608 16.8249L6.23207 10.0343L0.306796 6.17508H7.75442L10 0Z"
+            />
+          </svg>
+          <svg class="w-4 h-4 fill-current text-yellow-500" viewBox="0 0 20 20">
+            <path
+              d="M10 0L12.2456 6.17508H19.6932L13.7679 10.0343L15.6139 16.8249L10 13.3657L4.38608 16.8249L6.23207 10.0343L0.306796 6.17508H7.75442L10 0Z"
+            />
+          </svg>
+          <svg class="w-4 h-4 fill-current text-yellow-500" viewBox="0 0 20 20">
+            <path
+              d="M10 0L12.2456 6.17508H19.6932L13.7679 10.0343L15.6139 16.8249L10 13.3657L4.38608 16.8249L6.23207 10.0343L0.306796 6.17508H7.75442L10 0Z"
+            />
+          </svg>
+          <svg class="w-4 h-4 fill-current text-yellow-500" viewBox="0 0 20 20">
+            <path
+              d="M10 0L12.2456 6.17508H19.6932L13.7679 10.0343L15.6139 16.8249L10 13.3657L4.38608 16.8249L6.23207 10.0343L0.306796 6.17508H7.75442L10 0Z"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+    <p class="text-gray-800">COMMENTAIRE_DE_L_UTILISATEUR</p>
+  </div>
+  

354fd53a9f7920b3b9f0e9d4c7bab0032f98af06
Lafourcade-Noah <116797848+Lafourcade-Noah@users.noreply.github.com>
2023-05-30 18:45:43 +0200

    mention legale grille

--- src/pages/LegalView.vue
+++ src/pages/LegalView.vue
@@ -5,42 +5,55 @@
 <template>
   <div class="grid grid-cols-7 gap-20">
     <div class="col-span-2"> </div>
-    <div class="col-span-4">
+    <div class="  col-span-4 ">
       <h2 class=" font-europa-grotesk-sh text-green-dull "> Pour en savoir plus...</h2>
       <h2 class=" font-europa-grotesk-sh text-green-mid"> Nos mentions légales</h2>
       <p class=" text-green-dull mt-9"> Rue des Saveurs est le fruit d'un projet collectif au sein du département MMi
-        (Métiers
-        du Multimédia et d'Internet) de l'IUT Nord Franche-Comté. </p>
-        <h3 class=" font-poppins font-bold text-green-dull mt-9">Hébergeur</h3>
-      <p class=" text-green-mid mt-3">
-        SARL Rue des Saveurs <br>
-        4 Place Tharradin <br>
-        25200 Montbéliard <br>
-        France <br>
-      </p>
-    </div>
-    <div class="col-span-8">
-      <p class="">
-        Infomaniak <br>
-        9 Rue de la Gabelle <br>
-        1227 Carouge (Genève) <br>
-        Suisse <br>
-      </p>
-    </div>
-  </div>
+        (Métiers du Multimédia et d'Internet) de l'IUT Nord Franche-Comté. </p>
+      <div class="grid grid-cols-3 gap-4 mt-8">
+        <div>
+          <h3 class="font-poppins font-bold text-green-dull ">Propriétaire</h3>
+          <p class=" text-green-mid mt-3 ">
+            SARL Rue des Saveurs
+            4 Place Tharradin
+            25200 Montbéliard
+            France
+          </p>
+        </div>
+        <div>
+          <h3 class=" font-poppins font-bold text-green-dull">Hébergeur</h3>
+          <p class=" text-green-mid mt-3 ">
+            Infomaniak <br>
+            9 Rue de la Gabelle <br>
+            1227 Carouge (Genève) <br>
+            Suisse <br>
+          </p>
+        </div>
+      </div>
 
-  <div class="grid grid-cols-7 gap-20">
-    <div class="col-span-2"></div>
-    <div class="col-span-3">
-
-    </div>
-    <div class="col-span-2">
-      <p class="">
-        Infomaniak <br>
-        9 Rue de la Gabelle <br>
-        1227 Carouge (Genève) <br>
-        Suisse <br>
-      </p>
+      <div class="grid grid-cols-3 gap-4 mt-8">
+        <div>
+          <h3 class="font-poppins font-bold text-green-dull ">Intégration web</h3>
+          <p class=" text-green-mid mt-3 ">
+            Emilien Boucard
+            emil.boucard@gmail.com
+          </p>
+        </div>
+        <div>
+          <h3 class=" font-poppins font-bold text-green-dull">Hébergeur</h3>
+          <p class=" text-green-mid mt-3 ">
+            Zoe-Augustine Vicat <br>
+            zoe.a.vicat@gmail.com<br>
+          </p>
+        </div>
+        <div>
+          <h3 class=" font-poppins font-bold text-green-dull">Chargé de communication</h3>
+          <p class=" text-green-mid mt-3 ">
+            Noah Lafource <br>
+            noahlafourcade1@gmail.com<br>
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>

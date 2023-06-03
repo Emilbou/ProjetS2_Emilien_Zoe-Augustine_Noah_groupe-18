b65aebbe401dbb5de2735735326f57192d010fbe
emilbou <emilien.boucard@edu.univ-fcomte.fr>
2023-06-03 19:59:40 +0200

    import travail emilien card + grid

---
+++ src/components/CardProduit.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+
+</script>
+<template>
+
+    <div class="bg-white shadow-card-shadow p-3 flex flex-col">
+        <span class="bg-image-cover w-52 h-32"></span>
+        <h3 class="text-start font-bold text-base text-green-dull"> Citrons jaunes</h3>
+        <div class="flex flex-row items-center">
+        <span class="text-start font-normal text-xs text-green-dull"> 0,80€</span> <span class="text-start font-normal text-xs text-green-dull">/ unité</span>
+        </div>
+        <div class="flex flex-row items-center"><span><img src="icons/icon-localisation.svg" alt=""></span><span class="text-start font-normal text-xs text-green-mid"> 25200 Montbéliard</span></div>
+    </div>
+</template>
+
+<style>
+.bg-image-cover{
+background-image: url('https://picsum.photos/200/300');
+background-position: center;;
+background-repeat: no-repeat;
+background-size: cover;
+
+}
+
+</style>
--- src/pages/Ficherecette.vue
+++ src/pages/Ficherecette.vue
@@ -1,51 +1,28 @@
-
-
 <template>
-    <section class="mx-auto my-auto w-full flex">
-        <div class="grid grid-cols-12 gap-4">
-          <div class="col-span-4">
-            <img class="w-full aspect-w-1 aspect-h-1" src="https://picsum.photos/888/288" alt="">
-          </div>
-          <div class="col-span-8">
-    
-    
-        <GoogleMap :api-key="apiKey" class="w-2/3 h-72 "   :center="{lat: lat, lng: lng}" :zoom="15"> 
-        <Marker :options="{position: {lat: lat, lng: lng}}"/> </GoogleMap>
+  <section class="flex w-full justify-center px-32">
+  <section class="grille grid grid-cols-12 gap-4 ">
+    <div class="col-span-12 flex flex-wrap gap-4 ">
+      <Card class="col-span-2"></Card>
+      <Card class="col-span-2"></Card>
+      <Card class="col-span-2"></Card>
+      <Card class="col-span-2"></Card>
+      <Card class="col-span-2"></Card>
+      <Card class="col-span-2"></Card>
+      <Card class="col-span-2"></Card>
+      <Card class="col-span-2"></Card>
+      <Card class="col-span-2"></Card>
+      <Card class="col-span-2"></Card>
+      <Card class="col-span-2"></Card>
+      <Card class="col-span-2"></Card>
+      <Card class="col-span-2"></Card>
+      <Card class="col-span-2"></Card>
     </div>
-    </div>
-    </section>
-    
-    </template>
-    <script >
-    
-    import { GoogleMap, Marker} from 'vue3-google-map'
-    
-    
-    export default {
-    components: {
-        GoogleMap,
-        Marker
-    },
-    
-    data() {
-    return {
-        apiKey: "AIzaSyARZDmOKMyebql6OZpkhgaFVgS4GpTCsAs",
-        lat: null,
-        lng: null,
-    }
-    },
-    
-    
-      created() {
-        this.$getLocation()
-          .then((coordinates) => {
-            this.lat =coordinates.lat;
-            this.lng = coordinates.lng;
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
-    }
-    </script>
-    
+  </section>
+  
+  </section>
+</template>
+
+<script setup lang="ts">
+import Maps from '@/components/Maps.vue';
+import Card from '@/components/CardProduit.vue';
+</script>

1bb92074ce7ea14794f5cccaaf853c65347671c9
Lafourcade-Noah <116797848+Lafourcade-Noah@users.noreply.github.com>
2023-06-03 16:07:22 +0200

    card template panier

--- src/pages/panier.vue
+++ src/pages/panier.vue
@@ -6,10 +6,29 @@
     <main>
 
         <img class="  " src="src/assets/herbes.webp" alt="">
-        
+
         <div class="grid grid-cols-12 gap-20 bg">
             <h1 class="col-start-3 col-span-9 font-europa-grotesk-sh text-green-dull mt-3">Votre panier</h1>
-            <div class="col-span-7 bg-white"></div>
+            <div class="col-span-7 bg-white flex">
+                <img class=" p-5 w-200" src="src/assets/imgcard.webp" alt="">
+                <div>
+                    <div class="container flex justify-between ">
+                        <div class="flex items-end">
+                            <h1 class=" font-europa-grotesk-sh text-green-dull">Cerises</h1>
+                            <p class="font-poppins text-green-mid">X 20</p>
+                        </div>
+                        <img class=" items-end" src="src/assets/croix.svg" alt="">
+                    </div>
+                    <p class="font-poppins text-green-mid">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, quisquam modi? Labore, nostrum velit
+                        iste nulla debitis recusandae ipsum fugit, dolor deserunt hic incidunt culpa autem temporibus,
+                        minima adipisci accusamus?</p>
+                    <p class="font-poppins text-green-mid">X 20</p>
+                </div>
+            </div>
+
+
+
+
             <div class=" bg-white col-start-8 col-span-6 ">
                 <p>
                     En achetant sur Rue des Saveurs, vous vous engagez à respecter nos termes et conditions. Nous vous
@@ -75,4 +94,5 @@
         </div>
 
 
-</main></template>
+    </main>
+</template>

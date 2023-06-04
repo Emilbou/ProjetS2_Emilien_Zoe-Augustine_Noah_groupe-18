5d4479af122ef5b8841865c7cefbc6048e84698f
Lafourcade-Noah <116797848+Lafourcade-Noah@users.noreply.github.com>
2023-06-04 14:17:36 +0200

    index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,65 +4,45 @@
 </script>
 
 <template>
-    <header>
-        <h1 class="text-4xl">Test</h1>
-    </header>
+  <header>
+    
+  </header>
 
-    <main>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora recusandae et dolorem odit
-            beatae illo labore, praesentium quidem nulla eveniet, at voluptatem incidunt. Eaque explicabo,
-            vitae quibusdam vero quod atque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque possimus neque
-            amet sunt a consequatur, maiores molestiae enim sed odit officiis sapiente doloremque, non alias quo
-            reprehenderit. Quo, esse. At consequuntur dicta aliquid natus labore veritatis alias perferendis sunt rem
-            maxime, velit nemo officiis placeat reprehenderit quisquam facere optio eos ratione. Doloribus accusamus
-            repudiandae illum, numquam nesciunt tempora dolor in deleniti qui reiciendis nihil soluta dignissimos ex
-            adipisci officiis. Mollitia sint tempora officiis vitae quasi nisi fuga minima? Aliquam error tenetur dolores
-            molestias ratione cupiditate, perferendis numquam cumque officiis expedita aliquid amet recusandae? Esse
-            voluptates cum rerum sed necessitatibus odio voluptatem earum, velit, illum eaque blanditiis enim iusto tempore
-            iste vero fugiat? Commodi, dolorum. Deleniti laborum perspiciatis nobis, illum alias sed quia mollitia
-           reprehenderit quidem enim rerum excepturi, iure adipisci itaque asperiores, maxime ut libero consequatur. Amet,
-           cumque recusandae nobis itaque rem ducimus commodi quae, cupiditate earum nam iste minima quod. Ab, ipsa
-          aspernatur iusto deserunt quod fugiat tempore praesentium. Eligendi at aliquam consequuntur reprehenderit nobis
-         temporibus explicabo eum unde, expedita libero eveniet! Sed alias incidunt, dolorem hic rerum natus? Eligendi
-        pariatur modi nobis quidem laborum, soluta quasi amet exercitationem similique earum. Placeat, deserunt. Totam
-        sequi corrupti quasi voluptate sunt similique, sapiente magni rem esse eos ea provident repellat temporibus quas
-        facilis quam veniam placeat porro odio mollitia deserunt iusto quos fugit illum! Fuga delectus illum repellendus
-        deleniti sapiente quae veritatis neque voluptatem numquam libero consectetur tempora excepturi voluptas sint
-        dolore minus doloribus minima sunt quisquam quasi laboriosam, quam sed? Sit quidem ducimus non! Doloribus illum
-        fugiat eaque quia tempora magnam non nulla amet nihil consequuntur alias inventore, corporis, architecto
-        obcaecati aliquid sunt soluta, possimus minima distinctio voluptatum. Hic quasi possimus, ipsam ducimus eligendi
-        fugit beatae dolore quas velit quod dolor officia? Eum nobis labore a atque enim ipsa nostrum quas, iure sed ut
-        sit nemo saepe at? Error molestiae odio, vel, saepe repellendus iusto deleniti voluptatum ullam officia quis
-        laudantium totam delectus atque ducimus corporis aliquam nam praesentium. Commodi eligendi eaque, saepe porro
-        sapiente labore at nostrum assumenda eius quia, natus id pariatur maiores. Vero, maxime exercitationem
-        accusantium sint nulla eaque, vel nobis sed, deserunt dicta numquam saepe. Obcaecati quis tempore suscipit, illo
-        architecto hic ipsam. Perspiciatis iure aspernatur sequi amet, nemo soluta et porro architecto dolores assumenda
-        harum veritatis quisquam expedita eius quae ducimus iste quos alias repudiandae esse voluptatibus! Aliquid dolor
-        dicta odio molestias, perferendis tempore totam repellat similique accusantium optio eveniet autem vero culpa
-        magnam ad reprehenderit. Dicta nobis sunt voluptates consequatur assumenda. Aliquam deserunt sit est accusamus?
-        Aut aspernatur atque rerum expedita corporis sapiente? Commodi fugiat dignissimos voluptatum reiciendis aut qui
-        sequi, hic laborum minima quisquam, eos officiis facere ut repellendus explicabo consequuntur sed repellat
-        delectus. Atque delectus magni blanditiis laudantium animi vero reprehenderit, laborum non officia eveniet
-        voluptatum vitae repellendus dolor quasi sapiente! Laboriosam hic ex voluptates. Voluptatibus est ab dignissimos
-        quis dolore corporis at officia, mollitia blanditiis accusamus veritatis placeat, iste omnis earum natus velit,
-        consequatur beatae eos. Rem quam quo quos sint!
-    </p>
+  <main class="">
+    <img class=" col-span-12" src="src/assets/banner.webp" alt="">
+    <div class="grid grid-cols-12 gap-10">
+      <img class="col-start-5 col-span-4 -mt-20" src="src/assets/bienvenue.svg" alt="">
 
-    <div class="grid grid-cols-7 gap-20">
-  <div class="col-span-2"></div>
-  <div class="col-span-5">
-    <h1 class=" font-europa-grotesk-sh text-green-dull "> Pour en savoir plus...</h1>
-    <h1 class=" font-europa-grotesk-sh text-green-mid" > Nos mentions légales</h1>
-  </div>
-</div>
+      <section class=" shadow-card-shadow bg-white col-start-5 col-span-4">
+        <div class=" p-5">
+          <h2 class=" font-poppins text-green-mid font-bold text-2xl ">Découvrir nos produits <br> frais locaux</h2>
+          <div class=" flex justify-end p-0">
+          <img class=" -m-5 h-36 " src="src/assets/lemon_crop.svg" alt="">
+        </div></div>
+      </section>
+      <section class=" shadow-card-shadow bg-white col-start-5 col-span-4">
+        <div class=" p-5">
+          <h2 class=" font-poppins text-green-mid font-bold text-2xl ">Consulter les recettes de <br> nos utilisateurs</h2>
+          <div class=" flex justify-end p-0">
+          <img class=" -m-5 h-36 " src="src/assets/kiwi_crop.svg" alt="">
+        </div></div>
+      </section>
 
-<div>
-  <ul class=" mt-4" v-for="aliment in alimentliste" :key="aliment.id">
-          <RouterLink :to="{ name: 'id', params: { id: aliment.id } }">
-            <CardProduit v-bind="{ ...aliment }" />
-          </RouterLink>
-        </ul>
-        <CardProduit v-for="(aliment, index) in alimentliste" :key="index" :aliment="aliment"></CardProduit>
-</div>
-</main></template>
+      <div class="grid grid-cols-7 gap-20">
+        <div class="col-span-2"></div>
+        <div class="col-span-5">
+
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <ul class=" mt-4" v-for="aliment in alimentliste" :key="aliment.id">
+        <RouterLink :to="{ name: 'id', params: { id: aliment.id } }">
+          <CardProduit v-bind="{ ...aliment }" />
+        </RouterLink>
+      </ul>
+      <CardProduit v-for="(aliment, index) in alimentliste" :key="index" :aliment="aliment"></CardProduit>
+    </div>
+  </main>
+</template>

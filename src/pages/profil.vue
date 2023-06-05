51836bf79431d9cfb72d5b0e17ff362d3dd8697a
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2023-06-05 15:47:13 +0200

    avance page profil

--- src/pages/profil.vue
+++ src/pages/profil.vue
@@ -1,28 +1,85 @@
-<script lang="ts"></script>
-
 <template>
     <div class="bg-image-cover-profil w-full h-28"></div>
     <section>
         <div class="grid grid-cols-12 gap-4 w-full">
             <div class="col-start-3 col-span-1 -mt-5 flex flex-col">
                 <img src="@/assets/photo-woman-1.webp" alt="">
-                <div class="flex justify-evenly"><img src="@/assets/icones/icon-social-facebook.svg" alt=""><img src="@/assets/icones/icon-social-instagram.svg" alt=""><img src="@/assets/icones/icon-social-twitter.svg" alt=""></div>
+                <div class="flex justify-evenly">
+                    <img src="@/assets/icones/icon-social-facebook.svg" alt="">
+                    <img src="@/assets/icones/icon-social-instagram.svg" alt="">
+                    <img src="@/assets/icones/icon-social-twitter.svg" alt="">
+                </div>
             </div>
             <div class="flex-col flex col-start-4 col-span-5">
-            <div class="col-start-4 items-start flex flex-row w-full py-3"> <h1 class="text-green-dull text-4xl font-medium">Melina Moreau</h1> <button class="font-bold text-beige text-base bg-green-dull px-8 py-1 rounded-full ml-5">S'abonner</button> <span class="bg-green-dull rounded-full ml-5 p-2"><img src="@/assets/icones/icon-link.svg" alt=""></span></div>
-        </div>
-        <div class="col-start-4 col-span-4 -mt-16">
-
-            <div class="flex flex-row justify-between w-full"><span class="text-green-dull font-bold text-base">10 produits</span><span class="text-green-dull font-bold text-base">10 produits</span><span class="text-green-dull font-bold text-base">10 produits</span><span class="text-green-dull font-bold text-base">10 produits</span></div>
-        </div>
-        <p class="col-start-4 col-span-5 -mt-8  text-base text-green-dull font-normal">Bienvenue sur mon profil ! 
-<br>
-    J’ai pour passion le jardinage, c’est pourquoi je souhaite vous la partager en vendant mes productions ici.
-    Mes ventes sont pour la plupart permanentes tant que c’est la saison, n’hésitez pas à aller voir mes produits !</p>
+                <div class="col-start-4 items-start flex flex-row w-full py-3">
+                    <h1 class="text-green-dull text-4xl font-medium">Melina Moreau</h1>
+                    <button
+                        class="font-bold text-beige text-base bg-green-dull px-8 py-1 rounded-full ml-5">S'abonner</button>
+                    <span class="bg-green-dull rounded-full ml-5 p-2"><img src="@/assets/icones/icon-link.svg"
+                            alt=""></span>
+                </div>
+                <div class="flex flex-row justify-start col-start-4 col-span-5 items-center">
+                    <img src="@/assets/icones/star-rating-full.svg" alt="">
+                    <img src="@/assets/icones/star-rating-full.svg" alt="">
+                    <img src="@/assets/icones/star-rating-full.svg" alt="">
+                    <img src="@/assets/icones/star-rating-full.svg" alt="">
+                    <img src="@/assets/icones/star-rating-full.svg" alt="">
+                    <span class="text-base font-normal text-green-mid pl-3">(16 avis)</span>
+                </div>
+            </div>
+            <div class="col-start-4 col-span-4 -mt-4">
+                <div class="flex flex-row justify-between w-full">
+                    <span class="text-green-dull font-bold text-base">10 produits</span>
+                    <span class="text-green-dull font-bold text-base">10 produits</span>
+                    <span class="text-green-dull font-bold text-base">10 produits</span>
+                    <span class="text-green-dull font-bold text-base">10 produits</span>
+                </div>
+            </div>
+            <div class="col-start-4 col-span-5">
+                <p class="text-base text-green-dull font-normal">Bienvenue sur mon profil !
+                    <br>
+                    J’ai pour passion le jardinage, c’est pourquoi je souhaite vous la partager en vendant mes productions
+                    ici.
+                    Mes ventes sont pour la plupart permanentes tant que c’est la saison, n’hésitez pas à aller voir mes
+                    produits !
+                </p>
+            </div>
+            <div class="col-start-2 col-end-11 flex justify-start items-center mt-10">
+                <h3 class="text-xl font-bold text-green-dull">Autres produits de Melina Moreau</h3>
+                <button class="pl-6 text-green-mid text-xs font-normal">voir tous les produits</button>
+            </div>
+            <div class="col-start-2 col-end-12 gap-4 flex justify-between items-center">
+                <img class="col-span-2 w-full" src="@/assets/Rectangle 194.webp" alt="">
+                <img class="col-span-2 w-full" src="@/assets/Rectangle 194.webp" alt="">
+                <img class="col-span-2 w-full" src="@/assets/Rectangle 194.webp" alt="">
+                <img class="col-span-2 w-full" src="@/assets/Rectangle 194.webp" alt="">
+                <img class="col-span-2 w-full" src="@/assets/Rectangle 194.webp" alt="">
+            </div>
+            <div class="col-start-2 col-end-11 flex justify-start items-center mt-10">
+                <h3 class="text-xl font-bold text-green-dull">Autres produits de Melina Moreau</h3>
+                <button class="pl-6 text-green-mid text-xs font-normal">voir tous les produits</button>
+            </div>
+            <div class="col-start-2 col-end-12 gap-4 flex justify-between items-center">
+                <img class="col-span-2 w-full" src="@/assets/Rectangle 194.webp" alt="">
+                <img class="col-span-2 w-full" src="@/assets/Rectangle 194.webp" alt="">
+                <img class="col-span-2 w-full" src="@/assets/Rectangle 194.webp" alt="">
+                <img class="col-span-2 w-full" src="@/assets/Rectangle 194.webp" alt="">
+                <img class="col-span-2 w-full" src="@/assets/Rectangle 194.webp" alt="">
+            </div>
+            <div class="col-start-2 col-end-11 flex justify-start items-center mt-10">
+                <h3 class="text-xl font-bold text-green-dull">Autres produits de Melina Moreau</h3>
+                <button class="pl-6 text-green-mid text-xs font-normal">voir tous les produits</button>
+            </div>
+            <div class="col-start-2 col-end-12 gap-4 flex justify-between items-center">
+                <img class="col-span-2 w-full" src="@/assets/Rectangle 194.webp" alt="">
+                <img class="col-span-2 w-full" src="@/assets/Rectangle 194.webp" alt="">
+                <img class="col-span-2 w-full" src="@/assets/Rectangle 194.webp" alt="">
+                <img class="col-span-2 w-full" src="@/assets/Rectangle 194.webp" alt="">
+                <img class="col-span-2 w-full" src="@/assets/Rectangle 194.webp" alt="">
+            </div>
         </div>
     </section>
 </template>
-
 
 <style>
 .bg-image-cover-profil {
@@ -31,7 +88,6 @@
     top: 0;
     background-image: url('@/assets/herbes.webp');
     background-position: center;
-    ;
     background-repeat: no-repeat;
     background-size: cover;
 }</style>

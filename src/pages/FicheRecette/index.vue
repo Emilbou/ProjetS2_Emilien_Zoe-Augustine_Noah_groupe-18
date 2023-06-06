5d26af56ff667b071382d699415f7c1558dd2119
Lafourcade-Noah <116797848+Lafourcade-Noah@users.noreply.github.com>
2023-06-06 15:14:50 +0200

    ajout page recette

---
+++ src/pages/FicheRecette/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { allProduit } from '@/backend';
+import CardProduit from '@/components/CardProduit.vue';
+const produitliste = await allProduit();
+console.log(produitliste)
+</script>
+
+
+<template>
+  <section class="flex w-full justify-center px-24">
+    <section class="grille grid grid-cols-12 gap-4 ">
+      <div class="col-span-12 flex flex-wrap gap-4 ">
+
+        <ul class=" mt-4" v-for="produits of produitliste" :key="produits.id">
+          <RouterLink
+           :to="{ 
+            name: 'FicheProduit-id',
+             params: { id: produits.id } }">
+            <CardProduit class=" col-span-2" v-bind="{ ...produits }" />
+          </RouterLink>
+        </ul>
+      </div>
+    </section>
+
+  </section>
+</template>
+
